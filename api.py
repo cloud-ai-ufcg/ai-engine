7acf731c0cd7369503efa92c714074edf1a08017
Nathan Pedroza <nathan.pedroza@lsd.ufcg.edu.br>
2025-06-30 16:44:51 -0300

    chore: import runtime config from yaml file

--- api.py
+++ api.py
@@ -76,8 +76,6 @@ async def root():
     return {"status": "healthy", "message": "AI Engine API is running"}
 
 
-
-
 async def apply_recommendations(result_df):
     """
     Sends a POST request to the Actuator service endpoint with the recommendations
@@ -89,7 +87,9 @@ async def apply_recommendations(result_df):
     """
     try:
         # Apply recommendations
-        recommendations_json = json.dumps(result_df.to_dict(orient="records"), ensure_ascii=False)
+        recommendations_json = json.dumps(
+            result_df.to_dict(orient="records"), ensure_ascii=False
+        )
         async with aiohttp.ClientSession() as session:
             async with session.post(
                 f"http://{app_state.config['actuator']['host']}:{app_state.config['actuator']['port']}/{app_state.config['actuator']['route']}",
@@ -168,7 +168,9 @@ async def start():
     # Start the scheduler to run every 30 seconds after the first execution
     global scheduler_thread
     schedule.every(SCHEDULER_INTERVAL).seconds.do(lambda: asyncio.run(fetch_metrics()))
-    scheduler_thread = threading.Thread(target=run_scheduler, name="scheduler-thread", daemon=True)
+    scheduler_thread = threading.Thread(
+        target=run_scheduler, name="scheduler-thread", daemon=True
+    )
     scheduler_thread.start()
 
     logger.info(format_message("Recommendations are running", icon="🚀", color="GREEN"))
@@ -190,4 +192,9 @@ async def stop():
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8083, reload=False)
+    uvicorn.run(
+        "api:app",
+        host=app_state.config["server"]["host"],
+        port=app_state.config["server"]["port"],
+        reload=app_state.config["server"]["reload"],
+    )
